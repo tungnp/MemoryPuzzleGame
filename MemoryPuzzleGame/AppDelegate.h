4d678a8300136dc0e95597e5d414f1b2662240dd
tung nguyen <tungnp92@gmail.com>
2013-04-04 22:37:25 +0700

    Initial Commit

---
+++ MemoryPuzzleGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MemoryPuzzleGame
+//
+//  Created by stevie nguyen on 4/4/13.
+//  Copyright (c) 2013 tung nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MemoryPuzzleGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MemoryPuzzleGame
+//
+//  Created by stevie nguyen on 4/4/13.
+//  Copyright (c) 2013 tung nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
